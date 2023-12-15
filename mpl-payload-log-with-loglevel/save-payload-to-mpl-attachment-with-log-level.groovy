e3492a9cd4457df2bd243a78a4c894e290bbe5dc
Vadim Klimov <Vadim.Klimov@gmail.com>
2023-12-15 16:22:49 +0000

    save-payload-to-mpl-attachment-with-log-level.groovy: Use properties SAP_MPL_LogLevel_*

--- mpl-payload-log-with-loglevel/save-payload-to-mpl-attachment-with-log-level.groovy
+++ mpl-payload-log-with-loglevel/save-payload-to-mpl-attachment-with-log-level.groovy
@@ -2,7 +2,7 @@ import com.sap.gateway.ip.core.customdev.util.Message
 
 Message processData(Message message) {
 
-    String logLevel = message.getProperty('SAP_MessageProcessingLogConfiguration').logLevel.toString()
+    String logLevel = message.getProperty('SAP_MPL_LogLevel_Overall')
 
     if (logLevel in ['DEBUG', 'TRACE']) {
         messageLogFactory.getMessageLog(message)?.addAttachmentAsString('Message body', message.getBody(String), 'text/plain')
